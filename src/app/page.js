901be971b5962f6aacad77db664729732564c0dc
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-03-26 02:46:09 +0600

    login & registration page added

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl text-red-500 mt-10 text-center">
-        healthcare appointment systems
-      </h1>
+      
     </>
   );
 }
---
+++ src/components/GoogleLogin/GoogleLogin.jsx
@@ -0,0 +1,26 @@
+import { FcGoogle } from "react-icons/fc";
+
+
+const GoogleLogin = () => {
+  const handleGoogleSignIn = async () => {
+   
+  };
+  return (
+    <div>
+      <div
+        onClick={handleGoogleSignIn}
+        className="flex cursor-pointer items-center justify-center mt-4 text-gray-600  border rounded-lg   hover:bg-gray-200 "
+      >
+        <div className="px-4 py-2 text-2xl">
+          <FcGoogle />
+        </div>
+
+        <span className="w-5/6 px-4 py-3 font-bold text-center">
+          Sign in with Google
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default GoogleLogin;
