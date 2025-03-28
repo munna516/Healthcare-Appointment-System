5247a3b22454b04f02de0b1eadb8d9ad8d79556f
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-03-28 23:45:32 +0600

    become a doctor route design completed

--- src/app/(pages)/became-doctor/page.jsx
+++ src/app/(pages)/became-doctor/page.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import DoctorForm from "./DoctorForm";
+import DoctorHero from "./DoctorHero";
+import OurRulesAndPolicy from "./OurRulesAndPolicy";
 
 export default function BecameDoctor() {
-  return <div>BecameDoctor</div>;
+  return (
+    <div className="mb-12 md:mb-24">
+      {/* Hero section */}
+      <DoctorHero/>
+      <div className="flex gap-6 flex-col lg:flex-row justify-between max-w-7xl w-full mx-auto">
+          <OurRulesAndPolicy/>
+          <DoctorForm/>
+      </div>
+    </div>
+  );
 }
