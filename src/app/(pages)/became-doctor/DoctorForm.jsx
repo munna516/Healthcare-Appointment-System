8545e72e5ff83af2cb48d883d92090f66bd33f0f
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-03-29 02:08:25 +0600

    changes on whole file

--- src/app/(pages)/became-doctor/DoctorForm.jsx
+++ src/app/(pages)/became-doctor/DoctorForm.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { Input } from "@components/ui/input";
 import { Label } from "@components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@components/ui/select";
 import { Textarea } from "@components/ui/textarea";
 import { Button } from "@components/ui/button";
+import { Input } from "@components/ui/input";
 // import { DatePicker } from "@/components/ui/date-picker";
 
 export default function DoctorForm() {
