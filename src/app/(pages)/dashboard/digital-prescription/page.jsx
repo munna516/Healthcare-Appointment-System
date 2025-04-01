1c74d588dc8fde3773f582c9ccb96f8b246b8ead
rrishiddh <rrishiddh@gmail.com>
2025-04-01 16:02:24 +0600

    dashboard all route added

---
+++ src/app/(pages)/dashboard/digital-prescription/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const digitalPrescription = () => {
+    return (
+        <div>
+            <p>digital-prescription by dr</p>
+        </div>
+    );
+};
+
+export default digitalPrescription;
---
+++ src/app/(pages)/dashboard/give-review/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <p>give-review</p>
+        </div>
+    );
+};
+
+export default page;
