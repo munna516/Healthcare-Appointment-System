6f9822f468d86c9b200029a62ec48b34b2554a47
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-04-05 17:39:05 +0600

    some modification added in some section

--- src/app/(pages)/dashboard/components/SidebarContent.jsx
+++ src/app/(pages)/dashboard/components/SidebarContent.jsx
@@ -8,7 +8,7 @@ import { useSession } from "next-auth/react";
 
 
 export default function SidebarContent() {
-  const role = "admin";
+  const role = "doctor";
   // const { data, status } = useSession();
 
   return (
